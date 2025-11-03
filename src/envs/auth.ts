df1a59ccfd1bd87c255fa15b2b6da2fecd9fca2b
yongkang <2607881950@qq.com>
2025-11-03 18:28:32 +0800

    feat(auth): 添加NextAuth和GitHub OAuth认证配置

---
+++ src/envs/auth.ts
@@ -0,0 +1,68 @@
+import { createEnv } from '@t3-oss/env-nextjs';
+import { z } from 'zod';
+
+declare global {
+  // eslint-disable-next-line @typescript-eslint/no-namespace
+  namespace NodeJS {
+    interface ProcessEnv {
+      // ===== Clerk ===== //
+      NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY?: string;
+      CLERK_SECRET_KEY?: string;
+      CLERK_WEBHOOK_SECRET?: string;
+
+      // ===== Next Auth ===== //
+      NEXT_AUTH_SECRET?: string;
+      NEXT_AUTH_DEBUG?: string;
+
+      // Github
+      GITHUB_CLIENT_ID?: string;
+      GITHUB_CLIENT_SECRET?: string;
+    }
+  }
+}
+
+export const getAuthConfig = () => {
+  return createEnv({
+    client: {
+      NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY: z.string().optional(),
+      /**
+       * whether to enabled clerk
+       */
+      NEXT_PUBLIC_ENABLE_CLERK_AUTH: z.boolean().optional(),
+
+      NEXT_PUBLIC_ENABLE_NEXT_AUTH: z.boolean().optional(),
+    },
+    server: {
+      // Clerk
+      CLERK_SECRET_KEY: z.string().optional(),
+      CLERK_WEBHOOK_SECRET: z.string().optional(),
+
+      // NEXT-AUTH
+      NEXT_AUTH_SECRET: z.string().optional(),
+      NEXT_AUTH_DEBUG: z.boolean().optional().default(false),
+
+      // Github
+      GITHUB_CLIENT_ID: z.string().optional(),
+      GITHUB_CLIENT_SECRET: z.string().optional(),
+    },
+
+    runtimeEnv: {
+      // Clerk
+      NEXT_PUBLIC_ENABLE_CLERK_AUTH: !!process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY,
+      NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY: process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY,
+      CLERK_SECRET_KEY: process.env.CLERK_SECRET_KEY,
+      CLERK_WEBHOOK_SECRET: process.env.CLERK_WEBHOOK_SECRET,
+
+      // Next Auth
+      NEXT_PUBLIC_ENABLE_NEXT_AUTH: process.env.NEXT_PUBLIC_ENABLE_NEXT_AUTH === '1',
+      NEXT_AUTH_SECRET: process.env.NEXT_AUTH_SECRET,
+      NEXT_AUTH_DEBUG: !!process.env.NEXT_AUTH_DEBUG,
+
+      // Github
+      GITHUB_CLIENT_ID: process.env.GITHUB_CLIENT_ID,
+      GITHUB_CLIENT_SECRET: process.env.GITHUB_CLIENT_SECRET,
+    },
+  });
+};
+
+export const authEnv = getAuthConfig();
---
+++ src/libs/next-auth/auth.config.ts
@@ -0,0 +1,58 @@
+import type { Session, User, SessionStrategy } from 'next-auth';
+import type { JWT } from 'next-auth/jwt';
+import GitHub from 'next-auth/providers/github';
+
+import { getAuthConfig } from '@/envs/auth';
+
+const { NEXT_AUTH_DEBUG, NEXT_AUTH_SECRET } = getAuthConfig();
+
+// Notice this is only an object, not a full Auth.js instance
+const authConfig = {
+  callbacks: {
+    async jwt({ token, user }: { token: JWT; user?: User }) {
+      if (user?.id) {
+        token.userId = user.id;
+      }
+      return token;
+    },
+    async session({ session, token, user }: { session: Session; token: JWT; user?: User }) {
+      if (session.user) {
+        if (user) {
+          (session.user as unknown as { id?: string }).id = user.id as string;
+        } else {
+          (session.user as unknown as { id?: string }).id = (token.userId ?? (session.user as unknown as { id?: string }).id) as string;
+        }
+      }
+      return session;
+    },
+  },
+  debug: NEXT_AUTH_DEBUG,
+  pages: {
+    error: '/next-auth/error',
+    signIn: '/next-auth/signin',
+  },
+  providers: [
+    GitHub({
+      authorization: { params: { scope: 'read:user user:email' } },
+      clientId: (process.env.GITHUB_CLIENT_ID ?? process.env.AUTH_GITHUB_ID) ?? '',
+      clientSecret: (process.env.GITHUB_CLIENT_SECRET ?? process.env.AUTH_GITHUB_SECRET) ?? '',
+      profile: (profile) => {
+        return {
+          email: profile.email,
+          id: profile.id.toString(),
+          image: profile.avatar_url,
+          name: profile.name,
+          providerAccountId: profile.id.toString(),
+        };
+      },
+      allowDangerousEmailAccountLinking: true,
+    }),
+  ],
+  secret: NEXT_AUTH_SECRET,
+  session: {
+    strategy: 'jwt' as SessionStrategy,
+  },
+  trustHost: process.env?.AUTH_TRUST_HOST ? process.env.AUTH_TRUST_HOST === 'true' : true,
+};
+
+export default authConfig;
